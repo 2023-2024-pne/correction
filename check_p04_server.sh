08cf472160db40aee1c3330f8ae38f14db0bbc01
Sergio Arroutbi <sergio.arroutbi@urjc.es>
2024-05-15 19:21:55 +0200

    Add P04 server error URL test (#2)
    Signed-off-by: Sergio Arroutbi <sergio.arroutbi@urjc.es>

--- check_p04_server.sh
+++ check_p04_server.sh
@@ -6,3 +6,5 @@ curl http://127.0.0.1:8080/info/T
 curl http://127.0.0.1:8080/index.html
 curl http://127.0.0.1:8080/
 curl http://127.0.0.1:8080/error.html
+
+curl http://127.0.0.1:8080/unexistant | grep -i error
