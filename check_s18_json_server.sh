9434c13d84a7e72cc3fa11466acb29243ee3a1e9
Sergio Arroutbi <sergio.arroutbi@urjc.es>
2024-05-19 12:28:11 +0200

    Fix s18 JSON server tests (#19)
    Signed-off-by: Sergio Arroutbi <sergio.arroutbi@urjc.es>

--- check_s18_json_server.sh
+++ check_s18_json_server.sh
@@ -2,5 +2,6 @@
 test -z "${1}" && PORT=8080
 curl 2>/dev/null "http://localhost:${PORT}/listusers" || echo "ERROR[listusers]"
 curl 2>/dev/null "http://localhost:${PORT}/listusers" | jq || echo "ERROR[jq]"
+curl 2>/dev/null "http://localhost:${PORT}/unexistant" | grep -i error || echo "ERROR[error]"
 python3 json-client-1.py || echo "ERROR[client1]"
 python3 json-client-2.py || echo "ERROR[client2]"
--- check_s18_json_server_e2.sh
+++ check_s18_json_server_e2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-curl 2>/dev/null "http://localhost:8080/" || echo "ERROR[index]"
-curl 2>/dev/null "http://localhost:8080/listusers" || echo "ERROR[listusers]"
-curl 2>/dev/null "http://localhost:8080/listusers" | jq || echo "ERROR[jq]"
+test -z "${1}" && PORT=8080
+curl 2>/dev/null "http://localhost:${PORT}/" || echo "ERROR[index]"
+curl 2>/dev/null "http://localhost:${PORT}/listusers" || echo "ERROR[listusers]"
+curl 2>/dev/null "http://localhost:${PORT}/listusers" | jq || echo "ERROR[jq]"
+curl 2>/dev/null "http://localhost:${PORT}/unexistant" | grep -i error || echo "ERROR[error]"
 python3 json-client-e2.py || echo "ERROR[client2]"
