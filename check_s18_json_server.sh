3b11b90c63667918c0afcfc47d70b0c11ad0adee
Sergio Arroutbi <sergio.arroutbi@urjc.es>
2024-05-19 11:56:19 +0200

    Fix S18 scripts (#18)
    Signed-off-by: Sergio Arroutbi <sergio.arroutbi@urjc.es>

--- check_s18_json_server.sh
+++ check_s18_json_server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-curl 2>/dev/null "http://localhost:8080/listusers" || echo "ERROR[listusers]"
-curl 2>/dev/null "http://localhost:8080/listusers" | jq || echo "ERROR[jq]"
+test -z "${1}" && PORT=8080
+curl 2>/dev/null "http://localhost:${PORT}/listusers" || echo "ERROR[listusers]"
+curl 2>/dev/null "http://localhost:${PORT}/listusers" | jq || echo "ERROR[jq]"
 python3 json-client-1.py || echo "ERROR[client1]"
 python3 json-client-2.py || echo "ERROR[client2]"
