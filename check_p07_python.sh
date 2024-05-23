0f8af01a47db37147cc607dd574782fddcfda66e
Sergio Arroutbi <sergio.arroutbi@urjc.es>
2024-05-23 17:17:40 +0200

    Dump extra information (#21)
    Signed-off-by: Sergio Arroutbi <sergio.arroutbi@urjc.es>

--- check_p07_python.sh
+++ check_p07_python.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 EXPECTED_FILES="e1.py e2.py e3.py e5.py"
+export PYTHONPATH=../P00:../P01:../P03:../P06:${PYTHONPATH}
 for i in ${EXPECTED_FILES};
 do
-    PYTHONPATH=../P01 python3 ./"${i}"
+    echo "------------ ${i} --------------"
+    python3 ./"${i}"
+    echo "--------------------------------"
 done
 
+echo "------------ e4.py --------------"
 /usr/bin/expect <(cat <<EOF
 	spawn /usr/bin/python3 ./e4.py
 	expect ":"
@@ -12,3 +16,4 @@ done
 	interact
 EOF
 )
+echo "---------------------------------"
