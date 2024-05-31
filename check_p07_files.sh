e67cb0242fbf54d5cb6ec7687ef99aa942818647
Sergio Arroutbi <sergio.arroutbi@urjc.es>
2024-05-31 18:55:25 +0200

    Fix issue reported by shellcheck
    Signed-off-by: Sergio Arroutbi <sergio.arroutbi@urjc.es>

--- check_p07_files.sh
+++ check_p07_files.sh
@@ -2,7 +2,7 @@
 EXPECTED_FILES="e1.py e2.py e3.py e4.py e5.py"
 for i in ${EXPECTED_FILES};
 do
-    test -f $i && echo "$i:OK" || echo "$i:FAIL"
+    test -f "${i}" && echo "$i:OK" || echo "$i:FAIL"
 done
 echo "------------------------"
 find . -iname "*.html" -print
