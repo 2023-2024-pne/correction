16d64059810c79bfd0cb4269c8c3dbba9ee249b9
Sergio Arroutbi <sergio.arroutbi@urjc.es>
2024-05-31 18:58:01 +0200

    Include stash and pop for rebase script (#23)
    Signed-off-by: Sergio Arroutbi <sergio.arroutbi@urjc.es>

--- rebase_all.sh
+++ rebase_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-FILE="../2023-2024-pne/github-accounts/github-accounts.md"
+FILE="../github-accounts/github-accounts.md"
 for user in $(cat "${FILE}" | grep -v 'Yes ' | grep -v 'No ' |grep -v '| Name' | grep -v '\-' | grep '|' | awk -F "|" '{print $3}' | tr -d ' ');
 do
-	pushd "${user}" && git pull --rebase ||:
+	pushd "${user}" && (git stash; git pull --rebase; git stash pop) ||:
 	popd
 done
